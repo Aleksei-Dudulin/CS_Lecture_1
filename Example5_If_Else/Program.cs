8d661964e236da8a93c13ea7f63244b2c1631b54
Aleksei <dulittlefamily@yandex.ru>
2022-10-12 01:10:28 +0300

    Add Example5_If_Else

---
+++ Example5_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine ("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "хуй")
+{
+    Console.WriteLine("Ура, наконец-то Хуй на месте");
+}
+else
+{
+    Console.Write("Ну здарова, ");
+    Console.WriteLine(username);
+}
