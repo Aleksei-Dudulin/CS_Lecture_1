62a620267033cb757e820571baccbed15e2b5f3b
Aleksei <dulittlefamily@yandex.ru>
2022-10-12 22:44:04 +0300

    Add Example_Dog

---
+++ Example_Dog/Program.cs
@@ -0,0 +1,23 @@
+﻿// direction of dog`s movement = Dir, distance = D, first friend speed = Vf1, second friend speed = Vf2, dog speed = Vd, time = T
+int Dir = 2, D = 1000, Vf1 = 1, Vf2 = 2, Vd = 6, count = 0;
+
+while (D > 2)
+{
+    if (Dir == 1)
+    {
+        int T = D / (Vf1 + Vd);
+        Dir = 2;
+        D = D - (Vf1 + Vf2) * T;
+    }
+    else
+    {
+        int T = D / (Vf2 + Vd);
+        Dir = 1;
+        D = D - (Vf1 + Vf2) * T;
+    }
+    count++;
+}
+
+Console.WriteLine("Собака пробежала между друдьями ");
+Console.Write(count);
+Console.Write(" раз.");
